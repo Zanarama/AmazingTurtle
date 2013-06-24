e9a2114960cdbf9489aef969f6918533db0aa04f
Suzanne <zanarama17@gmail.com>
2013-06-24 14:56:54 -0400

    A sample of the maze starting to be solved.

---
+++ solution.py
@@ -0,0 +1,17 @@
+from turtle import*
+import maze
+
+# A place for students to create their solution
+# A quick fix for some students not being able to create files
+# On the WCBOE computers
+
+maze.drawMaze()
+forward (50)
+right (65)
+forward (60)
+left (110)
+forward (60)
+color ("purple")
+right(90)
+forward (100)
+exitonclick()
